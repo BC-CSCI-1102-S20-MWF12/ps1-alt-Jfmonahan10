5d467872e76fec009bc6d248c2960b6ca03e5d62
James Monahan <jamesmonahan@Jamess-MacBook-Pro.local>
2020-01-16 14:36:41 -0500

    In the file HelloWorld.java, I created my own set of comments that state my name, this class, the date, the purpose of the file, and what I changed within the file. In the file, I only changed line 11 to a new greeting.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,14 @@
-// This is how you write a comment.
-// This is another comment.
+// James Monahan
+// CSCI1102
+// January 16, 2020
+// The HelloWorld Class returns the simple greeting "What's up dawg?".
+/* The only code that I changed, was the text that was outputed for the user
+in line 11*/
 
 public class HelloWorld {
 
   public static void main (String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("What's up dawg?");
   }
 
 }
